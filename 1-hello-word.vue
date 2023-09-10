617eeb4792579bdb2acf1160082ff35b49ab0274
Trung Nguyen <85091105+trungnd19@users.noreply.github.com>
2023-09-10 21:28:34 +0700

    Create 1-hello-word.vue

---
+++ 1-hello-word.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue"
+const msg = ref("Hello World")
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+// Read more
+// https://vuejs-challenges.netlify.app/questions/1-hello-word/README.html
