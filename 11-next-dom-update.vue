d841c0afafd12c3ff4b43d686eae6916f1c550a0
Trung Nguyen <85091105+trungnd19@users.noreply.github.com>
2023-09-11 16:20:05 +0700

    Create 11-next-dom-update.vue

---
+++ 11-next-dom-update.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { nextTick, ref } from "vue"
+
+const count = ref(0)
+const counter = ref(null)
+
+async function increment() {
+  count.value++
+
+  nextTick(() => {
+    console.log(+counter.value.textContent === 1)  
+  })
+
+  /**
+   * DOM is not yet updated, how can we make sure that the DOM gets updated
+   * Make the output be true
+  */
+}
+</script>
+
+<template>
+  <button ref="counter" @click="increment">
+    {{ count }}
+  </button>
+</template>
+
+// Read more
+// https://vuejs.org/api/general.html#nexttick
