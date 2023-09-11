f02e87b3a8c45f974b976651d4e0d3328396b965
Trung Nguyen <85091105+trungnd19@users.noreply.github.com>
2023-09-11 09:26:18 +0700

    Create 323-prop-validation.vue

---
+++ 323-prop-validation.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  type: {
+    validator(value: string) {
+      return ['primary', 'ghost', 'dashed', 'link', 'text', 'default'].includes(value)
+    },
+    default: 'default'
+  },
+})
+</script>
+
+<template>
+  <button>Button</button>
+</template>
+
+// Read more
+// https://vuejs-challenges.netlify.app/questions/323-prop-validation/README.html
+// https://vuejs.org/guide/components/props.html#prop-validation
+// https://qiita.com/kawabata324/items/5c8d3a789e080a426285
