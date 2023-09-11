cbf3f1b85510a5f7118231c1535b9b29908f47a2
Trung Nguyen <85091105+trungnd19@users.noreply.github.com>
2023-09-11 09:33:56 +0700

    Create 13-dom-portal.vue

---
+++ 13-dom-portal.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+const msg = "Hello World"
+
+</script>
+
+<template>
+  <!-- Renders it to a child element of the `body` -->
+  <Teleport to="body">{{ msg }}</Teleport>
+</template>
+
+
+// Read more
+// https://vuejs.org/guide/built-ins/teleport.html#basic-usage
